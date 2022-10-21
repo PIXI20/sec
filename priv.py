73be8a01d1a8a7b12f51c6522cd4574b72df78e3
Pixi <116006744+PIXI20@users.noreply.github.com>
2022-10-22 01:50:33 +0700

    Update priv.py

--- priv.py
+++ priv.py
@@ -515,7 +515,7 @@ def crack(idf,pwv):
 	global loop,ok,cp
 	prog.update(des,description=f"[white]crack {str(loop)}/{len(id2)} OK-: [bold green]{ok}[/] CP-: [bold yellow]{cp}[/]")
 	prog.advance(des)
-	ua = random.choice(ugen2)
+	ua = random.choice(redmi)
 	ses = requests.Session()
 	for pw in pwv:
 		try:
